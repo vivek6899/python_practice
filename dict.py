1f3021edd747038b9263e19724d422d3a9d431fe
vivek6899 <vivektrivedi2345vt@gmail.com>
2021-05-29 14:25:56 +0530

    done

---
+++ dict.py
@@ -0,0 +1,25 @@
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+
+x = thisdict.keys()
+
+print(x)
+
+
+
+
+
+
+
+# thisdict = {
+#   "brand": "Ford",
+#   "model": "Mustang",
+#   "year": 1964
+# }
+
+# x = thisdict.values()
+
+# print(x)
---
+++ removeitemlist.py
@@ -0,0 +1,11 @@
+# thislist = ["apple", "banana", "cherry"]
+# thislist.remove("banana")
+# print(thislist)
+
+# thislist = ["apple", "banana", "cherry"]
+# thislist.pop(1)
+# print(thislist)
+
+thislist = ["apple", "banana", "cherry"]
+thislist.clear()
+print(thislist)
