7aa1907bec871d6773d38922854ae3079f67fd29
vivek6899 <vivektrivedi2345vt@gmail.com>
2021-05-29 13:16:25 +0530

    done

---
+++ bool.py
@@ -0,0 +1,10 @@
+print(bool(False))
+print(bool(None))
+print(bool(0))
+print(bool(""))
+print(bool(()))
+print(bool([]))
+print(bool({}))
+
+
+#everything will be false
---
+++ list.py
@@ -0,0 +1,6 @@
+# mylist = ["apple", "banana", "cherry"]
+
+# print(type(mylist))
+
+thislist = list(("apple", "banana", "cherry"))  #rounded bracket
+print(thislist)
