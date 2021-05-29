10544ab09537d8d0ebcb544ef245e4c34c79af18
vivek6899 <vivektrivedi2345vt@gmail.com>
2021-05-29 12:46:11 +0530

    done

---
+++ stringCount.py
@@ -0,0 +1,4 @@
+txt ="I love apples, apple are my favorite fruit"
+x = txt.count("apple")
+
+print(x)
---
+++ stringformat.py
@@ -0,0 +1,10 @@
+# age = 22
+# txt = "My name is Vivek, and I am {}"
+# print(txt.format(age))
+
+
+quantity = 3
+itemno = 567
+price = 49.95
+myorder = "I want to pay {2} dollars for {0} pieces of item {1}."
+print(myorder.format(quantity, itemno, price))
